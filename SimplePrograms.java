94a0bacbaa35d4b9594b8701f6ef903eb8ad9c85
TheProgrammer565 <hemapriyavadivel25@gmail.com>
2022-02-27 11:02:10 +0530

    Simple_Programs

---
+++ SimplePrograms.java
@@ -0,0 +1,107 @@
+
+/*
+import java.util.Scanner;
+public class Main{
+public static void main(String[]args){
+Scanner io=new Scanner(System.in);
+System.out.println("Inputtheradiusofthecircle:");
+double radius=io.nextDouble();
+System.out.println("Perimeteris="+(2*radius*Math.PI));
+System.out.println("Areais="+(Math.PI*radius*radius));
+}
+}
+*/
+  
+  /*
+  public class Exercise11{
+private static final double radius=7.5;
+public static void main(String[] args){
+  double perimeter=2*Math.PI*radius;
+double area=Math.PI*radius*radius;
+System.out.println("Perimeteris="+perimeter);
+System.out.println("Areais="+area);
+}
+}
+*/
+  
+  /*
+ public class FizzBuzz{
+public void getTheNumberDetails(int num){
+if(num<=0){
+System.out.println("Pleaseenteranumberabove0");
+}
+else if(num>=200000){
+System.out.println("Pleaseenteranumberbelow200000");
+} 
+else{
+     for(inti=1;i<=num;i++){
+            if(i%3==0&&i%5==0){
+                   System.out.println("FizzBuzz");
+}
+            else if(i%3==0){
+                     System.out.println("Fizz");
+}
+            else if(i%5==0){
+                    System.out.println("Buzz");
+}
+            else{
+                     System.out.println(i);
+            }
+        }
+     }
+}
+public static void main(String args[]){
+FizzBuzz fb=new FizzBuzz();
+fb.getTheNumberDetails(100000);
+}
+}
+
+*/
+  
+  /* importjava.io.*;
+importjava.util.*;
+publicclassExercise1_2{
+publicstaticvoidmain(String[]args){
+Scanners=newScanner(System.in);
+System.out.println("Enterthevalueofx:");
+intx=s.nextInt();
+  System.out.println("Enterthevalueofy:");
+inty=s.nextInt();
+System.out.println("Enterthevalueofz:");
+intz=s.nextInt();
+intresult=0;
+if(x>y&&x>z){
+System.out.println("xisthelargestvalue");
+}
+elseif(y>x&&y>z){
+System.out.println("yisthelargestvalue");
+}
+else{
+System.out.println("zisthelargestvalue");
+}
+}
+}
+*/
+  
+  
+/*
+  importjava.util.*;
+publicclassExercise1_1{
+publicstaticvoidmain(String[]args){
+System.out.println("Entertheradius:");
+Scanners=newScanner(System.in);
+double radius=s.nextDouble();
+doubleperimeter;
+doublearea;
+if(radius<=0){
+System.out.println("Pleaseenteranon-zeropositivenumber");
+}
+else{
+perimeter=2*Math.PI*radius;
+area=Math.PI*radius*radius;
+System.out.println("Perimeteris="+perimeter);
+System.out.println("Areais="+area);
+}
+}
+}
+*/
